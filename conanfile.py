c181f1e1ae3948b615f1e2a90a4f7dc49889e768
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-05 14:47:21 +0200

    move checks to validate()

--- conanfile.py
+++ conanfile.py
@@ -37,6 +37,8 @@ class CityhashConan(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+
+    def validate(self):
         if self.settings.compiler == "Visual Studio" and self.options.shared:
             raise ConanInvalidConfiguration("cityhash does not support shared builds with Visual Studio")
 
